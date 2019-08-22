78f7ab321fc50f475f0f95329dda6510c125e317
YUZHU CHAI <yuzhupoemhead@gmail.com>
2019-08-22 12:20:13 -0500

    header, able to type

--- litshare/src/Header/index.js
+++ litshare/src/Header/index.js
@@ -1,23 +1,52 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { Grid, Input } from 'semantic-ui-react'
+import { Route, Link } from 'react-router-dom'
+import { Grid, Input, Button, Form } from 'semantic-ui-react'
 
 
-const Header = () => {
-	return (
-		<Grid>
-			<Grid.Colum width={4}>
-				<Input icon='search' placeholder='Search...' />
-			</Grid.Colum>
-			<Grid.Colum width={6}>
-				<h1> LITSHARE </h1>
-			</Grid.Colum>
-			<Grid.Colum width={4}>
-				<a>login</a>
-				<a>logout</a>			
-			</Grid.Colum>
-		</Grid>
-	)
+class Header extends React.Component{
+	constructor(){
+		super()
+		this.state={
+			keyword:''
+		}
+	}
+
+	handleSubmit=(e) =>{
+		e.preventDefault()
+
+	}
+
+	handleChange=(e) => {
+		this.setState({
+			keyword:e.target.value
+		})
+	}
+
+	render(){
+		console.log(this.state);	
+		return (
+				<Grid>
+					<Grid.Column width={4}>
+						<Form onSubmit={this.handleSubmit}>
+							<Input icon='search' name='keyword' placeholder='Search...' value={this.state.value} onChange={this.handleChange}/>
+							<Button>find</Button>
+						</Form>
+					</Grid.Column>
+					<Grid.Column width={8}>
+						<h1> LITSHARE </h1>
+					</Grid.Column>
+					<Grid.Column width={4}>
+						<a href='/users/register'>register</a><br/>
+ 						<a href='/users/login'>login</a><br/>
+						<a>username</a><br/>
+						<a>logout</a>			
+					</Grid.Column>
+				</Grid>
+		)
+	}
 }
 
-export default Header 
+export default Header;
+
+// <Link to='/users/register'>register</Link><br/>
+// <Link to='/users/login'>login</Link><br/>
