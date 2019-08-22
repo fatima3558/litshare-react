f46527b83e62fc5629157b20bf6722aa6f4d6b89
YUZHU CHAI <yuzhupoemhead@gmail.com>
2019-08-22 11:47:28 -0500

    pull master

--- litshare/src/Header/index.js
+++ litshare/src/Header/index.js
@@ -1,4 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Grid, Input } from 'semantic-ui-react'
 
 
+const Header = () => {
+	return (
+		<Grid>
+			<Grid.Colum width={4}>
+				<Input icon='search' placeholder='Search...' />
+			</Grid.Colum>
+			<Grid.Colum width={6}>
+				<h1> LITSHARE </h1>
+			</Grid.Colum>
+			<Grid.Colum width={4}>
+				<a>login</a>
+				<a>logout</a>			
+			</Grid.Colum>
+		</Grid>
+	)
+}
+
+export default Header 
