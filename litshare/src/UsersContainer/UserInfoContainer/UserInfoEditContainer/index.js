a8df5d8d597b9cee29e02983e9a597e2f027aae2
Fatima Gomez <fatima3558@gmail.com>
2019-08-23 17:37:44 -0500

    form submission ready for db query

--- litshare/src/UsersContainer/UserInfoContainer/UserInfoEditContainer/index.js
+++ litshare/src/UsersContainer/UserInfoContainer/UserInfoEditContainer/index.js
@@ -1,25 +1,69 @@
 import React from 'react';
 
 class UserInfoEditContainer extends React.Component {
-	constructor() {
+	constructor(props) {
 		super()
 		this.state = {
+			username: props.user.username,
+			email: props.user.email,
+			bio: props.user.bio,
+			zipcode: props.user.zipcode
+		}
+	}
 
+	handleChange = (e) => {
+		this.setState({
+			[e.currentTarget.name]: e.currentTarget.value
+		})
+	}
+
+	handleSubmit = async (e) => {
+		e.preventDefault()
+		try {
+			console.log(this.props, "this is props in UserInfoEditContainer")
+			console.log(this.state);
+			// write database query to update user here
+		} catch(err) {
+			console.log(err);
 		}
 	}
 
 	render() {
 		return(
-			<form>
-				<h1>{this.props.username}'s Account Information</h1>
-				<h4>Name:</h4><br/>
-				<input placeholder={this.props.user.username}></input>
-				<h4>Email:</h4><br/>
-				<input placeholder={this.props.user.email}></input>
-				<h4>Bio:</h4> <br/>
-				<input placeholder={this.props.user.bio}></input>
-				<h4>Zip Code:</h4><br/>
-				<input placeholder={this.props.user.zipcode}></input>
+			<form onSubmit={this.handleSubmit}>
+				<h1>{this.props.user.username}'s Account Information</h1>
+
+				<p>Name:</p><br/>
+				<input 
+					type='text'
+					name='username'
+					value={this.state.username}
+					onChange={this.handleChange}
+				/>
+
+				<p>Email:</p><br/>
+				<input 
+					type='text'
+					name='email'
+					value={this.state.email}
+					onChange={this.handleChange}
+				/>
+
+				<p>Bio:</p> <br/>
+				<textarea 
+					name='bio'
+					value={this.state.bio}
+					onChange={this.handleChange}
+				/>
+
+				<p>Zip Code:</p><br/>
+				<input 
+					type='text'
+					name='zipcode'
+					value={this.props.user.zipcode}
+					onChange={this.handleChange}
+				/><br/>
+				<button>Submit</button>
 			</form>
 		)
 	}
