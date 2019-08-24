04aa3447b303050ce6b79476788777b83f6a326d
YUZHU CHAI <yuzhupoemhead@gmail.com>
2019-08-24 16:16:31 -0500

    deletecopy

--- litshare/src/CopyList/index.js
+++ litshare/src/CopyList/index.js
@@ -14,6 +14,24 @@ class CopyList extends React.Component {
 		this.getCopy()
 	}
 
+
+	deleteOneCopy = async (copyId) => {
+		console.log(copyId,"<-------should be the copyid!!!");
+		const url = `http://localhost:8000/books/${this.props.bookid}/copy/${copyId}`
+		const deleteOneCopyResponse = await fetch(url, {
+			method: 'DELETE',
+			credentials: 'include'
+		})
+		const prasedResponse = await deleteOneCopyResponse.json()
+		console.log(prasedResponse,"<---------deleteOneCopy response ");
+		const newCopies = this.state.copies.filter(copy => copy.id !== copyId)
+		console.log(newCopies,"<----newcopies");
+		this.setState({
+			copies : newCopies
+		})
+	}
+
+
 	getCopy = async () => {
 		const url = `http://localhost:8000/books/${this.props.bookid}/copy`
 		const getCopiesResponse = await fetch(url,{
@@ -21,22 +39,24 @@ class CopyList extends React.Component {
         	credentials: 'include'
 		})
 
-		console.log(getCopiesResponse);
+		// console.log(getCopiesResponse);
 	    const prasedResponse = await getCopiesResponse.json()
-	    console.log(prasedResponse,"<00000000parsedresposne ");
+	    // console.log(prasedResponse,"<00000000parsedresposne ");
 
 	    this.setState({
 	    	copies: prasedResponse.data
 	    })
 	}
 
+
+
 	render(){
-		// console.log(this.state,"<----state in copy list ");
+		console.log(this.state,"<----state in copy list ");
 		const singlecopy = this.state.copies.map(copy => {
 			return (
 				<Grid.Row key={copy.id} align='middle'>
 					<p><a>{copy.owner_id.username}</a> <br/>condition: {copy.condition}<br/>edition: {copy.edition}<br/>price: {copy.price}</p><br/>
-					<Button>Delete</Button>
+					<Button onClick={this.deleteOneCopy.bind(null, copy.id)}>Delete</Button>
 					<Button>Edit</Button>
 					<Button>REQUEST</Button>
 				</Grid.Row>
