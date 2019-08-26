318507646016298983278c16bebbc76230df6689
Fatima Gomez <fatima3558@gmail.com>
2019-08-26 13:24:15 -0500

    added hearts to footer

--- litshare/src/Footer/index.js
+++ litshare/src/Footer/index.js
@@ -5,15 +5,19 @@ function Footer(props){
 	return(
 		<Sticky>
 			<Grid attached='bottom'>
-				<Grid.Column width={6} align='middle'>
+				<Grid.Column width={4} align='middle'>
 					{props.loggedIn ? 
 						<a onClick={props.toggleUpload.bind(null)}>Upload a Book</a> : 
 						<a href='/users'>Log In or Register</a>
 					}
 				</Grid.Column>
 				
-				<Grid.Column width={6} align='middle'>
+				<Grid.Column width={8} align='middle'>
 					<p>© 2019 <i>LITSHARE</i> by Chris, Fatima, Yuzhu</p>
+				</Grid.Column>
+
+				<Grid.Column width={4} align='middle'>
+					<p>&hearts; &hearts; &hearts; &hearts; &hearts; &hearts; &hearts;</p>
 				</Grid.Column>
 			</Grid>
 		</Sticky>
