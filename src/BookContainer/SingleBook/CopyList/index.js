5274f6504595e51cb066bdee7330e072357c2377
YUZHU CHAI <yuzhupoemhead@gmail.com>
2019-08-28 09:54:15 -0500

    style copy list

--- src/BookContainer/SingleBook/CopyList/index.js
+++ src/BookContainer/SingleBook/CopyList/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Divider, Grid, Button } from 'semantic-ui-react'
+import { Divider, Card, Button } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 class CopyList extends React.Component{
@@ -14,26 +14,32 @@ class CopyList extends React.Component{
 	}
 
 	render(){
-		console.log(this.props,"<-----props in copy list");
-		const singlecopy = this.props.copies.map(copy => {
+		// console.log(this.props,"<-----props in copy list");
+		const singlecopy = this.props.copies.map((copy,i) => {
+			const title = 'copy#'+{i}
 			return (
-				<Grid.Row key={copy.id} align='left'>
-					<p><a onClick={this.showUser.bind(null, copy.owner_id)}> {copy.owner_id.username}</a> <br/>condition: {copy.condition}<br/>edition: {copy.edition}<br/>price: {copy.price}<br/>rental time: {copy.rental_time} days</p>
-					<Button.Group>
-					<Button size='mini' onClick={this.props.deleteOneCopy.bind(null, copy.id)}>Delete</Button>
-					<Button size='mini' onClick={this.props.editCopy.bind(null, copy)}>Edit</Button>
-					<Button size='mini' >REQUEST</Button>
-					</Button.Group>
-				</Grid.Row>
+				<Card key={copy.id} >
+					<Card.Content>
+						<Card.Header>
+							<a onClick={this.showUser.bind(null, copy.owner_id)}> {copy.owner_id.username}</a>
+						</Card.Header>
+						<Card.Description>
+							<p> condition: {copy.condition}<br/>edition: {copy.edition}<br/>price: {copy.price}<br/>rental time: {copy.rental_time} days</p>
+						<Card.Content extra>
+							<Button size='mini' onClick={this.props.deleteOneCopy.bind(null, copy.id)}>Delete</Button>
+							<Button size='mini' onClick={this.props.editCopy.bind(null, copy)}>Edit</Button>
+							<Button size='mini' >REQUEST</Button>
+						</Card.Content>
+						</Card.Description>
+					</Card.Content>
+				</Card>
 			)
 		})
 		// the buttons above shoule be able to toggle according to who is logged in 
 		return(
 			<div>
-				<Grid>
-					<h4>these guys have this book! hit them up</h4>
-					{singlecopy}
-				</Grid>
+				<h4>these guys have this book! hit them up</h4>
+				{singlecopy}
 			</div>
 		)
 	}
